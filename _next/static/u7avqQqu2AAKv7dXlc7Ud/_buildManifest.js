6b571dbf550a352fd9b83b4bb60581226df10bcb
Débora Guardado <deboraguardado@MacBook-Air-de-Debora.local>
2026-04-19 14:42:48 -0600

    Deploy 2

--- _next/static/u7avqQqu2AAKv7dXlc7Ud/_buildManifest.js
+++ _next/static/u7avqQqu2AAKv7dXlc7Ud/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nextli-app//_next/:path+",
+        "destination": "/nextli-app/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
